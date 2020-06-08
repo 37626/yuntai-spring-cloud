2b80821164f8a00099627fb4666aae7a5698a5cf
袁野mustang <yuanyea@zbj.com>
2020-06-08 14:35:24 +0800

    init

---
+++ auth-web/src/main/java/com/yuntai/web/MainWebApplication.java
@@ -0,0 +1,42 @@
+package com.yuntai.web;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import springfox.documentation.spring.web.SpringfoxWebMvcConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@EnableSwagger2
+@ConditionalOnClass(SpringfoxWebMvcConfiguration.class)
+@MapperScan("com.yuntai.web.mapper")
+public class MainWebApplication {
+
+    private static Logger logger= LoggerFactory.getLogger( MainWebApplication.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run( MainWebApplication.class, args );
+        Environment env = application.getEnvironment();
+        logger.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t"+
+                        "Doc: \thttp://{}:{}/doc.html\n"+
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+    }
+
+}
